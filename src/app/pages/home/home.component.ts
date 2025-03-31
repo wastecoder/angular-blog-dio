24e674ce8d1615871fbe7969fa206736a9810fe2
Paulino <paulinomendes03@gmail.com>
2025-03-31 18:24:04 -0300

    feat(big-card): cria a estrutura do card da notícia principal

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
+import { BigCardComponent } from "../../components/big-card/big-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitleComponent],
+  imports: [MenuTitleComponent, BigCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
