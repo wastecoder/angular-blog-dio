875ded84285b44e8a4b4103afa852f5edf60fc5a
Paulino <paulinomendes03@gmail.com>
2025-03-31 17:31:01 -0300

    feat(menu-title): cria a estrutura do título do blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SmallCardComponent],
+  imports: [RouterOutlet, SmallCardComponent, MenuTitleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
