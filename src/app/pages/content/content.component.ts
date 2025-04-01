b4e91337d310521e7b7d4599f36efb037daee96a
Paulino <paulinomendes03@gmail.com>
2025-04-01 03:07:53 -0300

    feat: cria a estrutura e adiciona binding ao content component

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+  styleUrl: './content.component.css',
 })
 export class ContentComponent {
-
+  photoUrl: string =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/800px-Placeholder_view_vector.svg.png';
+  contentTitle: string = 'Título padrão';
+  contentDescription: string = 'Descrição padrão';
 }
