24b7f0e476d843e7b9400e25cdf809a960759143
Paulino <paulinomendes03@gmail.com>
2025-04-01 06:24:40 -0300

    feat(fakeData): adiciona 4 postagens

--- src/app/data/fakeData.ts
+++ src/app/data/fakeData.ts
@@ -3,7 +3,7 @@ export const fakeData = [
     id: 1,
     photoUrl:
       'https://cdn.prod.website-files.com/66d16ac0a28e9fc29617fc2e/673b41ccea45ed2c55cf536b_angular-16-banner.jpg',
-    publishDate: 'Janeiro, 205',
+    publishDate: 'Janeiro, 2025',
     contentTitle: 'Nova versão do Angular',
     summary:
       'A nova versão do Angular chega com melhorias de desempenho e novos recursos para otimizar o desenvolvimento de aplicações web, tornando o framework ainda mais eficiente e fácil de usar.',
@@ -14,7 +14,7 @@ export const fakeData = [
     id: 2,
     photoUrl:
       'https://imageio.forbes.com/specials-images/imageserve/63abee1c40cdcf4a3fcc7319/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds',
-    publishDate: 'Janeiro, 205',
+    publishDate: 'Janeiro, 2025',
     contentTitle: 'O que é ChatGPT e o que ele é capaz de fazer?',
     summary:
       'Descubra como o ChatGPT revoluciona a interação entre humanos e máquinas, com capacidades de gerar texto e auxiliar em diversas tarefas complexas.',
@@ -24,7 +24,7 @@ export const fakeData = [
   {
     id: 3,
     photoUrl: 'https://highsales.digital/images/blog/o-que-e-web-design.jpg',
-    publishDate: 'Janeiro, 205',
+    publishDate: 'Janeiro, 2025',
     contentTitle: 'Tendências de design para sites em 2025',
     summary:
       'Explore as principais tendências de design para 2025 e como essas inovações podem transformar a experiência do usuário em websites modernos.',
@@ -35,11 +35,55 @@ export const fakeData = [
     id: 4,
     photoUrl:
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_vWPwXRYA63uAlNJ7JSu-zp8rUVDxymYU0Q&s',
-    publishDate: 'Março, 205',
+    publishDate: 'Março, 2025',
     contentTitle: 'Como o Spring Security fortalece suas aplicações?',
     summary:
       'Entenda como o Spring Security pode aumentar a segurança de suas aplicações Java, protegendo contra ameaças e garantindo uma arquitetura robusta.',
     description:
       'O Spring Security é uma poderosa framework de segurança que facilita a proteção de aplicações Java. Ele oferece uma série de recursos essenciais, como autenticação, controle de acesso e prevenção de ataques. Ao integrar o Spring Security em suas aplicações, é possível garantir a segurança de dados sensíveis, proteger endpoints e criar uma arquitetura robusta contra ameaças como injeção de SQL, CSRF e XSS. Essa integração ajuda desenvolvedores a criar sistemas mais seguros e resilientes.',
+  },
+  {
+    id: 5,
+    photoUrl:
+      'https://www.uplers.com/wp-content/uploads/2022/09/11-1-scaled.jpg',
+    publishDate: 'Janeiro, 2025',
+    contentTitle: 'Diferença entre React e React Native',
+    summary:
+      'Entenda as principais diferenças entre React e React Native, dois frameworks populares usados no desenvolvimento de interfaces, mas com objetivos distintos.',
+    description:
+      'Embora React e React Native compartilhem a mesma base de código, eles têm propósitos diferentes. React é uma biblioteca JavaScript focada no desenvolvimento de interfaces de usuário para web, enquanto o React Native é um framework usado para criar aplicativos móveis nativos para iOS e Android. Neste post, exploramos as diferenças chave entre os dois, abordando aspectos como desempenho, componentes nativos, e a experiência de desenvolvimento.',
+  },
+  {
+    id: 6,
+    photoUrl:
+      'https://www.verzani.com.br/wp-content/uploads/2020/12/5G-scaled.jpeg',
+    publishDate: 'Janeiro, 2025',
+    contentTitle: 'Como o 5G Pode Impactar o Desenvolvimento de Apps Móveis',
+    summary:
+      'Saiba como a chegada do 5G pode revolucionar o desenvolvimento de aplicativos móveis, oferecendo novas possibilidades para recursos e desempenho.',
+    description:
+      'O 5G promete trazer uma revolução ao desenvolvimento de aplicativos móveis, oferecendo maior velocidade, menor latência e conectividade mais estável. Com isso, as apps móveis poderão implementar recursos como streaming de vídeo de alta qualidade, realidade aumentada em tempo real e comunicação entre dispositivos em grande escala. Este post explora como as novas possibilidades do 5G podem impactar o design e a funcionalidade de apps móveis nos próximos anos.',
+  },
+  {
+    id: 7,
+    photoUrl:
+      'https://miro.medium.com/v2/resize:fit:1079/0*Vx537wvMkkIgRhW0.png',
+    publishDate: 'Janeiro, 2025',
+    contentTitle: 'Docker para Desenvolvimento Web',
+    summary:
+      'Descubra como o Docker pode simplificar o processo de desenvolvimento web, criando ambientes consistentes e otimizados para suas aplicações.',
+    description:
+      'Docker tem se tornado uma ferramenta essencial para desenvolvedores, permitindo a criação de containers que garantem ambientes de desenvolvimento consistentes em diferentes sistemas. Ao usar Docker no desenvolvimento web, é possível evitar problemas como "funciona na minha máquina" e melhorar o fluxo de trabalho, garantindo que sua aplicação rode da mesma forma em qualquer ambiente. Este artigo explora como configurar e utilizar o Docker em projetos de desenvolvimento web, destacando suas vantagens.',
+  },
+  {
+    id: 8,
+    photoUrl:
+      'https://public-images.interaction-design.org/literature/articles/materials/flTR2AEh0Xm8QMKevDdSR4x08XS9oDCs2FrMxpjj.jpg',
+    publishDate: 'Janeiro, 2025',
+    contentTitle: 'Design Responsivo: Melhores Práticas e Ferramentas',
+    summary:
+      'Aprenda sobre as melhores práticas e ferramentas para implementar design responsivo em websites, garantindo uma experiência otimizada para todos os dispositivos.',
+    description:
+      'O design responsivo é fundamental para garantir que os sites funcionem bem em dispositivos de diferentes tamanhos, desde desktops até smartphones. Utilizar técnicas como grids flexíveis, media queries e imagens adaptáveis pode melhorar a experiência do usuário. Este post explora as melhores práticas de design responsivo, além de destacar ferramentas como o Flexbox, o Grid Layout e frameworks como Bootstrap para ajudar a criar sites responsivos de forma eficiente e moderna.',
   },
 ];
