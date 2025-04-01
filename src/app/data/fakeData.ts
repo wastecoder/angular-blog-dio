8bb622f8ad5ebdba65a448233c1f8f3124feeb88
Paulino <paulinomendes03@gmail.com>
2025-04-01 04:46:35 -0300

    feat: melhora a estilização do content component e adiciona alguns dados melhores ao fakeData.ts

--- src/app/data/fakeData.ts
+++ src/app/data/fakeData.ts
@@ -5,8 +5,10 @@ export const fakeData = [
       'https://cdn.prod.website-files.com/66d16ac0a28e9fc29617fc2e/673b41ccea45ed2c55cf536b_angular-16-banner.jpg',
     publishDate: 'Janeiro, 205',
     contentTitle: 'Nova versão do Angular',
-    description:
+    summary:
       'A nova versão do Angular chega com melhorias de desempenho e novos recursos para otimizar o desenvolvimento de aplicações web, tornando o framework ainda mais eficiente e fácil de usar.',
+    description:
+      'A nova versão do Angular trouxe melhorias significativas de desempenho, além de novos recursos para otimizar o desenvolvimento de aplicações web. Com a introdução de funcionalidades como o Ivy renderer, a biblioteca de componentes atualizada e a melhor integração com ferramentas como o RxJS, o Angular se torna ainda mais eficiente e fácil de usar. Essas melhorias garantem uma experiência de desenvolvimento mais rápida e um código mais limpo e sustentável.',
   },
   {
     id: 2,
@@ -14,16 +16,20 @@ export const fakeData = [
       'https://imageio.forbes.com/specials-images/imageserve/63abee1c40cdcf4a3fcc7319/0x0.jpg?format=jpg&height=900&width=1600&fit=bounds',
     publishDate: 'Janeiro, 205',
     contentTitle: 'O que é ChatGPT e o que ele é capaz de fazer?',
-    description:
+    summary:
       'Descubra como o ChatGPT revoluciona a interação entre humanos e máquinas, com capacidades de gerar texto e auxiliar em diversas tarefas complexas.',
+    description:
+      'O ChatGPT é uma inteligência artificial avançada que utiliza modelos de linguagem para gerar texto de forma contextualizada e realista. Ele é capaz de realizar tarefas diversas, como responder perguntas, criar conteúdos criativos, fornecer suporte técnico e até auxiliar no desenvolvimento de código. Sua flexibilidade e capacidade de entender e gerar linguagem natural tornam o ChatGPT uma ferramenta poderosa para empresas e indivíduos em diversos campos.',
   },
   {
     id: 3,
     photoUrl: 'https://highsales.digital/images/blog/o-que-e-web-design.jpg',
     publishDate: 'Janeiro, 205',
     contentTitle: 'Tendências de design para sites em 2025',
-    description:
+    summary:
       'Explore as principais tendências de design para 2025 e como essas inovações podem transformar a experiência do usuário em websites modernos.',
+    description:
+      'Com a constante evolução da tecnologia, as tendências de design para sites em 2025 estão focadas na experiência do usuário e na funcionalidade intuitiva. O design responsivo continuará sendo essencial, assim como a integração de recursos de inteligência artificial para personalização. Além disso, a utilização de animações suaves, microinterações e layouts dinâmicos proporcionará um envolvimento maior com o usuário, criando experiências mais imersivas e agradáveis.',
   },
   {
     id: 4,
@@ -31,7 +37,9 @@ export const fakeData = [
       'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_vWPwXRYA63uAlNJ7JSu-zp8rUVDxymYU0Q&s',
     publishDate: 'Março, 205',
     contentTitle: 'Como o Spring Security fortalece suas aplicações?',
-    description:
+    summary:
       'Entenda como o Spring Security pode aumentar a segurança de suas aplicações Java, protegendo contra ameaças e garantindo uma arquitetura robusta.',
+    description:
+      'O Spring Security é uma poderosa framework de segurança que facilita a proteção de aplicações Java. Ele oferece uma série de recursos essenciais, como autenticação, controle de acesso e prevenção de ataques. Ao integrar o Spring Security em suas aplicações, é possível garantir a segurança de dados sensíveis, proteger endpoints e criar uma arquitetura robusta contra ameaças como injeção de SQL, CSRF e XSS. Essa integração ajuda desenvolvedores a criar sistemas mais seguros e resilientes.',
   },
 ];
