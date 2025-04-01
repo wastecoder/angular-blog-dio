41d023feec1a70f9d7cb077d66c6ffbf462d79a7
Paulino <paulinomendes03@gmail.com>
2025-04-01 06:42:35 -0300

    feat(vertical-card): adiciona input binding na imagem, data de publicação, título e resumo

--- src/app/components/vertical-card/vertical-card.component.ts
+++ src/app/components/vertical-card/vertical-card.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-vertical-card',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './vertical-card.component.html',
-  styleUrl: './vertical-card.component.css'
+  styleUrl: './vertical-card.component.css',
 })
 export class VerticalCardComponent {
+  @Input()
+  photoUrl: string = '';
 
+  @Input()
+  publishDate: string = 'Mês, Ano';
+
+  @Input()
+  cardTitle: string = 'Título padrão';
+
+  @Input()
+  cardSumary: string = 'Resumo padrão';
+
+  @Input()
+  id: string = '0';
 }
