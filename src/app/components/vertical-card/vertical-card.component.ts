54e5ad518b3d5d7fba0ad48a17f6cacfc06d2cff
Paulino <paulinomendes03@gmail.com>
2025-04-01 05:55:53 -0300

    feat(vertical-card): adiciona a estrutura do vertical-card component

---
+++ src/app/components/vertical-card/vertical-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-card',
+  imports: [],
+  templateUrl: './vertical-card.component.html',
+  styleUrl: './vertical-card.component.css'
+})
+export class VerticalCardComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { MenuTitleComponent } from '../../components/menu-title/menu-title.component';
 import { BigCardComponent } from '../../components/big-card/big-card.component';
 import { SmallCardComponent } from '../../components/small-card/small-card.component';
-import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+import { VerticalCardComponent } from '../../components/vertical-card/vertical-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent, MenuBarComponent],
+  imports: [
+    MenuTitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    VerticalCardComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
