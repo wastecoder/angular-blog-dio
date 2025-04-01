8dc7483e14af4918c55b789d7577b72534d03701
Paulino <paulinomendes03@gmail.com>
2025-03-31 23:16:33 -0300

    feat(big-card): adiciona binding de propriedades dinâmicas na imagem, título, data de publicação e descrição

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrl: './big-card.component.css',
 })
 export class BigCardComponent {
-
+  @Input()
+  photoUrl: string = '';
+  @Input()
+  publishDate: string = 'Mês, Ano';
+  @Input()
+  cardTitle: string = 'Título padrão';
+  @Input()
+  cardDescription: string = 'Descrição padrão';
 }
